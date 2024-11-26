5eb2dbf026827ce0a7394f5404bb877b60078c9f
Vuong <hienvuong2810@gmail.com>
2024-11-26 23:46:08 +0700

    update localStorage and set interval 60s

--- chrome-extension/src/background/index.ts
+++ chrome-extension/src/background/index.ts
@@ -68411,64 +68411,66 @@ async function getSportbookAG() {
       const result = await browser.scripting.executeScript({
         target: { tabId: tab.id },
         func: async () => {
-          const statsObject = {};
+          setInterval(() => {
+            const statsObject = {};
 
-          const extractPlayerStats = (eventBoxes, statType) => {
-            eventBoxes.forEach(eventBox => {
-              const playerName = eventBox.querySelector('.eventheading div')?.innerText.split(' - ')[0].trim();
+            const extractPlayerStats = (eventBoxes, statType) => {
+              eventBoxes.forEach(eventBox => {
+                const playerName = eventBox.querySelector('.eventheading div')?.innerText.split(' - ')[0].trim();
 
-              if (playerName) {
-                if (!statsObject[playerName]) {
-                  statsObject[playerName] = {};
-                }
-
-                const statRows = eventBox.querySelectorAll('.eventrow .row');
-                let foundStat = false;
-
-                statRows.forEach(row => {
-                  const marketDiv = row.querySelector('.market');
-
-                  if (marketDiv) {
-                    let [statValue, odds] = marketDiv.innerText.split('\n').map(item => item.trim());
-                    let statName = '';
-
-                    if (statValue.includes('O')) statName = 'OVER';
-                    if (statValue.includes('U')) statName = 'UNDER';
-
-                    const OU = statValue.replace('O', '').replace('U', '');
-
-                    if (!statsObject[playerName][statType]) {
-                      statsObject[playerName][statType] = {};
-                    }
-
-                    statsObject[playerName][statType] = {
-                      ...statsObject[playerName][statType],
-                      [statName]: odds.replace('(', '').replace(')', ''),
-                      '0U': OU,
-                    };
-
-                    foundStat = true;
+                if (playerName) {
+                  if (!statsObject[playerName]) {
+                    statsObject[playerName] = {};
                   }
-                });
 
-                if (!foundStat) {
-                  statsObject[playerName][statType] = {
-                    'O/U': 'N/A',
-                    OVER: 'N/A',
-                    UNDER: 'N/A',
-                  };
+                  const statRows = eventBox.querySelectorAll('.eventrow .row');
+                  let foundStat = false;
+
+                  statRows.forEach(row => {
+                    const marketDiv = row.querySelector('.market');
+
+                    if (marketDiv) {
+                      let [statValue, odds] = marketDiv.innerText.split('\n').map(item => item.trim());
+                      let statName = '';
+
+                      if (statValue.includes('O')) statName = 'OVER';
+                      if (statValue.includes('U')) statName = 'UNDER';
+
+                      const OU = statValue.replace('O', '').replace('U', '');
+
+                      if (!statsObject[playerName][statType]) {
+                        statsObject[playerName][statType] = {};
+                      }
+
+                      statsObject[playerName][statType] = {
+                        ...statsObject[playerName][statType],
+                        [statName]: odds.replace('(', '').replace(')', ''),
+                        '0U': OU,
+                      };
+
+                      foundStat = true;
+                    }
+                  });
+
+                  if (!foundStat) {
+                    statsObject[playerName][statType] = {
+                      'O/U': 'N/A',
+                      OVER: 'N/A',
+                      UNDER: 'N/A',
+                    };
+                  }
                 }
-              }
-            });
-          };
+              });
+            };
 
-          const pointEventBoxes = document.querySelector('.panel-body').querySelectorAll('.eventbox');
-          const reboundEventBoxes = document.querySelectorAll('.panel-body')[2].querySelectorAll('.eventbox');
+            const pointEventBoxes = document.querySelector('.panel-body').querySelectorAll('.eventbox');
+            const reboundEventBoxes = document.querySelectorAll('.panel-body')[2].querySelectorAll('.eventbox');
 
-          extractPlayerStats(pointEventBoxes, 'Points');
-          extractPlayerStats(reboundEventBoxes, 'Rebounds');
-
-          return statsObject;
+            extractPlayerStats(pointEventBoxes, 'Points');
+            extractPlayerStats(reboundEventBoxes, 'Rebounds');
+            localStorage.setItem(data, statsObject);
+            return statsObject;
+          }, 60000);
         },
       });
 
